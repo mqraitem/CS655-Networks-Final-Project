46a05417a384831129ed5b8ad0bbd97fd99d367e
mqraitem <mqraitem98@gmail.com>
2021-12-03 14:30:30 -0500

    first commit

---
+++ README.md
@@ -0,0 +1,8 @@
+to run the file: 
+
+* sudo python3 server-my.py 
+
+
+Then go to your favourite browser, and then type: localhost. 
+
+
---
+++ server-my.py
@@ -0,0 +1,12 @@
+#!/usr/bin/python3
+import os, sys
+from http.server import HTTPServer, CGIHTTPRequestHandler
+
+
+webdir = '.'   # where your html files and cgi-bin script directory live
+port   = 80    # default http://localhost/, else use http://localhost:xxxx/
+
+os.chdir(webdir)                                       # run in HTML root dir
+srvraddr = ("", port)                                  # my hostname, portnumber
+srvrobj  = HTTPServer(srvraddr, CGIHTTPRequestHandler)
+srvrobj.serve_forever()                                # run as perpetual daemon
