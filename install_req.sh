33a0c6dd37c93547ec7e147d6c26b89285700280
mqraitem <mqraitem98@gmail.com>
2021-12-07 00:42:39 -0500

    add install req

---
+++ install_req.sh
@@ -0,0 +1,5 @@
+sudo add-apt-repository universe
+sudo apt-get update
+sudo apt install python3-pip
+pip3 install torch==1.2.0+cpu torchvision==0.4.0+cpu -f https://download.pytorch.org/whl/torch_stable.html
+
