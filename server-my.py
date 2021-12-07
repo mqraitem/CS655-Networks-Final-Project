1309642c6faf807a431070d860fe26cc28b8d144
mqraitem <mqraitem98@gmail.com>
2021-12-07 11:13:57 -0500

    add multi threading server capability

--- server-my.py
+++ server-my.py
@@ -1,12 +1,21 @@
 #!/usr/bin/python3
 import os, sys
 from http.server import HTTPServer, CGIHTTPRequestHandler
+import SocketServer
 
+#webdir = '.'   # where your html files and cgi-bin script directory live
+#os.chdir(webdir)                                       # run in HTML root dir
+#srvraddr = ("0.0.0.0", port)                                  # my hostname, portnumber
+#srvrobj  = HTTPServer(srvraddr, CGIHTTPRequestHandler)
+#srvrobj.serve_forever()                                # run as perpetual daemon
 
-webdir = '.'   # where your html files and cgi-bin script directory live
-port   = 8080    # default http://localhost/, else use http://localhost:xxxx/
+class ThreadingCGIServer(SocketServer.ThreadingMixIn):
+    pass
 
-os.chdir(webdir)                                       # run in HTML root dir
-srvraddr = ("0.0.0.0", port)                                  # my hostname, portnumber
-srvrobj  = HTTPServer(srvraddr, CGIHTTPRequestHandler)
-srvrobj.serve_forever()                                # run as perpetual daemon
+server = ThreadingCGIServer(('', 8080), CGIHTTPRequestHandler)
+try:
+    while 1:
+        sys.stdout.flush()
+        server.handle_request()
+    except KeyboardInterrupt:
+        print ("Finished")
